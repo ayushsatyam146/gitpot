4140fe3d856ec82848407b5339be4e6feb195953
ayushsatyam146 <ayushsatyam146@gmail.com>
2023-12-24 01:23:30 +0530

    cleaning up writeFromTree

--- files/writeFromTree.go
+++ files/writeFromTree.go
@@ -1,46 +1,26 @@
 package files
 
 import (
-	"os"
-
 	utils "github.com/ayushsatyam146/gitpot/utils"
 )
 
 func WriteToGitpotFromTree(tree *Tree, path string) string {
 
-  // if _, err := os.Stat(path); os.IsNotExist(err) {
-  //   os.Mkdir(path, 0755)
-  // } 
+  // put a check to see if path exists
 
   TreeFileContent := "tree\n"
-  // make a git-tree wala file here sabse pehle 
   for _, child := range tree.children {
     if(!child.isDir) {
-      objectDirFileName, _ := utils.GetSHA1(child.value)
-      // objectDirContent := "blob\n" + utils.EncodeBase64(child.value)
-      objectDirContent := "blob\n" + string(child.value)
-      TreeFileContent += "blob " + objectDirFileName + " " + child.name + "\n"
-      os.MkdirAll(path + "/objects/" + objectDirFileName[0:2], 0755)
-      file, err := os.Create(path + "/objects/" + objectDirFileName[0:2] + "/" + objectDirFileName[2:])
-      if err != nil {
-        panic(err)
-      }
-      defer file.Close()
-      file.WriteString(objectDirContent)
+      utils.WriteToObjectsDir(path, child.value, false)
+      hash, _ := utils.GetSHA1(child.value)
+      TreeFileContent += "blob " + hash + " " + child.name + "\n"
     } else {
       hash := WriteToGitpotFromTree(child, path)
       TreeFileContent += "tree " + hash + " " + child.name + "\n"
     }
   }
 
-  finalHash, _ := utils.GetSHA1([]byte(TreeFileContent))
-  os.MkdirAll(path + "/objects/" + finalHash[0:2], 0755)
-  file, err := os.Create(path + "/objects/" + finalHash[0:2] + "/" + finalHash[2:])
-  if err != nil {
-    panic(err)
-  }
-  defer file.Close()
-  file.WriteString(TreeFileContent)
-
-  return finalHash
+  utils.WriteToObjectsDir(path, []byte(TreeFileContent), true)
+  hash, _ := utils.GetSHA1([]byte(TreeFileContent))
+  return hash
 }
