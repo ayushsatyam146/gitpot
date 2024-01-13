e60bda79c13f41b963f40ff8507fcdf6468916bc
ayushsatyam146 <ayushsatyam146@gmail.com>
2024-01-14 00:55:13 +0530

    fixed update index bug

--- branch/updateIndexWithTree.go
+++ branch/updateIndexWithTree.go
@@ -10,17 +10,15 @@ import (
 
 func UpdateIndexWithTree(treeHash string) {
 	treeContent := string(files.ReadFile("test/.gitpot/objects/" + treeHash[:2] + "/" + treeHash[2:]))
-	indexContent := ""
-	for index , line := range strings.Split(treeContent, "\n") {
-		if index != 0 {
-			indexContent += line + "\n"
-		}
-	}
-	file, err := os.Open("test/.gitpot/index")
+	stringContent := string(treeContent)
+	element := strings.Split(stringContent, "\n")[1:]
+	stringContent = strings.Join(element, "\n")
+	bytes := []byte(stringContent)
+
+	file, err := os.Create("test/.gitpot/index")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
-		return
 	}
 	defer file.Close()
-	file.WriteString(indexContent)
+	file.Write(bytes)
 }
